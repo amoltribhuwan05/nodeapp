508a077fa0de2e3a08d59d7775f7a5be4021f6be
amol <amoltribhuwan09@gmail.com>
2024-03-18 12:29:53 +0530

    18 commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
 <<<<<<< HEAD
- res.send('welcome 2');
+ res.send('welcome 3');
 =======
  res.send('welcome to node app server on jenkins used azure cloud 2nd time');
 >>>>>>> 74d3706d4df476ae94773af19a60e174883bfbe7
