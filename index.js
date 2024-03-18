3670d8b156b8baeb1187948428b43ad8cdac2152
amol <amoltribhuwan09@gmail.com>
2024-03-18 12:39:27 +0530

    18 commit

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
-<<<<<<< HEAD
- res.send('welcome 3');
-=======
- res.send('welcome to node app server on jenkins used azure cloud 2nd time');
->>>>>>> 74d3706d4df476ae94773af19a60e174883bfbe7
+ res.send('welcome 4 ');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
